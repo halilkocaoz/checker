673b6ff87d52c6c9c6b0fe548ead95cc72dbc3e7
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-08-18 20:43:39 +0300

    update monitor.go to complete set post values and headers methods

--- model/monitor.go
+++ model/monitor.go
@@ -2,6 +2,8 @@ package model
 
 import (
 	"net/http"
+
+	"github.com/halilkocaoz/upmo-checker/storage"
 )
 
 // upmo monitor
@@ -25,10 +27,44 @@ func (monitor *Monitor) DoRequest() (*http.Response, error) {
 
 // gets and sets header values from database
 func (m *Monitor) SetHeaders() {
-	m.Headers = nil
+	headers := make([]KVPair, 0)
+	db, _ := storage.UpMoDBConnection()
+	defer db.Close()
+
+	headerRows, _ := db.Query(`SELECT 
+	"Key", 
+	"Value"
+	FROM "Headers" 
+	WHERE ("DeletedAt" IS NULL AND "MonitorID" = $1) 
+	ORDER BY "CreatedAt"`, m.ID)
+	defer headerRows.Close()
+
+	for headerRows.Next() {
+		header := new(KVPair)
+		_ = headerRows.Scan(&header.Key, &header.Value)
+		headers = append(headers, *header)
+	}
+	m.Headers = headers
 }
 
 // gets and sets post form values from database
 func (m *Monitor) SetPostValues() {
-	m.PostValues = nil
+	postForms := make([]KVPair, 0)
+	db, _ := storage.UpMoDBConnection()
+	defer db.Close()
+
+	postFormRows, _ := db.Query(`SELECT
+	"Key", 
+	"Value"
+	FROM "PostForms" 
+	WHERE ("DeletedAt" IS NULL AND "MonitorID" = $1) 
+	ORDER BY "CreatedAt"`, m.ID)
+	defer postFormRows.Close()
+
+	for postFormRows.Next() {
+		postForm := new(KVPair)
+		_ = postFormRows.Scan(&postForm.Key, &postForm.Value)
+		postForms = append(postForms, *postForm)
+	}
+	m.PostValues = postForms
 }
