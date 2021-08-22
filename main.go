e94c62da8bcc520cb21d1a69e4d3a7e98e2ae194
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-08-22 19:43:51 +0300

    add getMonitorsByStatement

--- main.go
+++ main.go
@@ -1,4 +1,37 @@
 package main
 
+import (
+	"github.com/halilkocaoz/upsmo-checker/model"
+	"github.com/halilkocaoz/upsmo-checker/storage"
+)
+
 func main() {
+
+}
+
+func getMonitorsByStatement(statement string) ([]*model.Monitor, error) {
+	monitors := make([]*model.Monitor, 0)
+	db, _ := storage.UpsMoDBConn()
+	defer db.Close()
+
+	monitorRows, err := db.Query(statement)
+	if err != nil {
+		return monitors, err
+	}
+	defer monitorRows.Close()
+
+	for monitorRows.Next() {
+		mn := new(model.Monitor)
+
+		err = monitorRows.Scan(&mn.ID,
+			&mn.Host,
+			&mn.Method,
+			&mn.Region,
+			&mn.IntervalMs,
+			&mn.TimeoutMs,
+			&mn.CreatedAt)
+		monitors = append(monitors, mn)
+	}
+
+	return monitors, err
 }
