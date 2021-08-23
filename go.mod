1d4f97b1b13eff0f2b637e1a060b9b813e34e229
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-08-23 23:41:09 +0300

    add servicebus topic messaging

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/halilkocaoz/upsmo-checker
 
 go 1.16
 
-require github.com/lib/pq v1.10.2
+require (
+	github.com/lib/pq v1.10.2
+	github.com/michaelbironneau/asbclient v0.0.0-20190125194550-9c3367d7460e
+)
---
+++ stream/servicebus.go
@@ -0,0 +1,26 @@
+package stream
+
+import (
+	"log"
+	"os"
+
+	"github.com/michaelbironneau/asbclient"
+)
+
+var (
+	namespace string = os.Getenv("SERVICE_BUS_NAMESPACE")
+	keyvalue  string = os.Getenv("SERVICE_BUS_SHARED_ACCESS_KEY_VALUE")
+)
+
+func SendToServiceBus(topic string, message string) {
+	serviceBusClient := asbclient.New(asbclient.Topic, namespace, "RootManageSharedAccessKey", keyvalue)
+	err := serviceBusClient.Send(topic, &asbclient.Message{
+		Body: []byte(message),
+	})
+
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.Printf(`"%s" sent --> %s`, message, topic)
+	}
+}
